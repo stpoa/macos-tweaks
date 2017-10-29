e9e31fc3ac53df2911e6ae39beed220571771682
stpoa <stpoa@icloud.com>
2017-10-29 10:53:14 +0100

    Add quicklook plugins

--- files.sh
+++ files.sh
@@ -10,6 +10,9 @@ echo "• Making Directories..."
 mkdir ~/Github
 mkdir ~/Bitbucket
 
+echo "• Add quicklook plugins"
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql
+
 echo "• Making Symbolic links..."
 cd
 ln -s "Library/Mobile Documents/com~apple~CloudDocs" iCloud
